376aec47565575122bb323b037089fadda69210d
Bartłomiej Skopińśki <bartlomiejskopinski@gmail.com>
2018-10-12 01:45:45 +0200

    adding about page

--- routes/web.php
+++ routes/web.php
@@ -10,10 +10,15 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('/', function () {
+    return view('welcome');
+});
+
 
 Route::get('/', function () {
     return view('welcome');
 });
+
 
 Auth::routes();
 
