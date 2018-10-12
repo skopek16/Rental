2c2006b0aafaf94a420e658adaae05a0922a9495
Bartłomiej Skopińśki <bartlomiejskopinski@gmail.com>
2018-10-12 13:50:38 +0200

    update controllers for static pages

---
+++ app/Http/Controllers/PagesController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class PagesController extends Controller
+{
+    public function getIndex() 
+    {
+
+    }
+
+    public function getAbout() 
+    {
+
+    }                                                                       
+
+    public function getContact() 
+    {
+
+    }
+
+    
+}
